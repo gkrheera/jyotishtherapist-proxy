e05897f73cf49de7f098467cebf1f5eb5426ce90
gkrheera <aortic-wisest-6s@icloud.com>
2025-09-11 13:25:19 +0530

    Create proxy.js

---
+++ proxy.js
@@ -0,0 +1,14 @@
+{
+  "name": "jyotishtherapist-proxy",
+  "version": "1.0.0",
+  "description": "Proxy for ProKerala API to protect credentials.",
+  "main": "functions/proxy.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "GKRheera",
+  "license": "MIT",
+  "dependencies": {
+    "node-fetch": "^2.6.7"
+  }
+}
